e43fe6ccd94cafb58831f17dbac0585043fd5142
mc-b <marcel.bernet@mc-b.ch>
2026-01-05 09:05:51 +0100

    Anzahl VMs welche pro VM Host angelegt werden sollen

--- variables.tf
+++ variables.tf
@@ -86,4 +86,10 @@ variable "instance_type" {
   }
 }
 
+# nur lernmaas Modul!
+variable "vm_per_host" {
+  description = "Anzahl VMs welche pro VM Host angelegt werden sollen"
+  type        = number
+  default     = 1
+}
 
